2d857e2411a3ca5771e591fe1cf9263c4413e7a9
David Christensen <randomparity@gmail.com>
2026-05-09 13:10:27 -0700

    Simplify cropdetect track guard

--- crates/voom-cli/src/commands/workflow/process/pipeline.rs
+++ crates/voom-cli/src/commands/workflow/process/pipeline.rs
@@ -389,9 +389,10 @@ fn cropdetect_request_for_plan(
         };
         let crop = settings.crop.as_ref()?;
         let track_index = action.track_index?;
-        let track = file.tracks.iter().find(|track| {
-            track.index == track_index && track.track_type == voom_domain::media::TrackType::Video
-        })?;
+        let track = file
+            .tracks
+            .iter()
+            .find(|track| track.index == track_index && track.track_type.is_video())?;
         Some(CropDetectRequest {
             settings: crop.clone(),
             source: voom_ffmpeg_executor::cropdetect::CropDetectSource::new(
@@ -1038,7 +1039,7 @@ mod tests {
     }
 
     #[test]
-    fn crop_settings_for_plan_ignores_non_video_track_index() {
+    fn cropdetect_request_for_plan_ignores_non_video_track_index() {
         let mut file = MediaFile::new(std::path::PathBuf::from("/media/source.mkv"))
             .with_container(Container::Mkv)
             .with_tracks(vec![Track::new(0, TrackType::AudioMain, "aac".into())]);
